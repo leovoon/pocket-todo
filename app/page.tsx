bcc1ec7b3988ef330553dcc16821718289cbd5cb
Leo Voon <voonlihhaur@gmail.com>
2023-06-16 20:27:36 +0800

    Added revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import TodoDialog from "@/components/TodoDialog";
 import TodoList from "@/components/TodoList";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <>
