d67e274e4cd2f1cc4b3e451258dbb16e6c2b9d01
Leo Voon <voonlihhaur@gmail.com>
2023-06-16 18:13:51 +0800

    Add type, reupload lock file

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -11,7 +11,9 @@ export default async function TodoList() {
   return (
     <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-4 sm:text-2xl text-xl">
       {todos.length > 0 ? (
-        todos.map((todo) => <TodoListItem key={todo.id} {...todo} />)
+        todos.map((todo: { id: number; title: string; completed: boolean }) => (
+          <TodoListItem key={todo.id} {...todo} />
+        ))
       ) : (
         <div className="flex items-center justify-between p-4 border-b">
           <span>No todos yet</span>
